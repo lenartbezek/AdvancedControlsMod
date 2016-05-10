be783df163688bd1ef56a3c192f8ff74d50a43c2
Lenart Bezek <lench4991@gmail.com>
2016-05-10 14:42:34 +0200

    Split Axis.cs file

---
+++ AdvancedControlsMod/Controls/Interface.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace AdvancedControlsMod.Controls
+{
+    public interface Control
+    {
+        float Min { get; set; }
+        float Max { get; set; }
+        float Center { get; set; }
+        bool OnlyPositive { get; set; }
+        void Apply(float value);
+        void Draw();
+    }
+
+    public interface ControlGroup
+    {
+        Dictionary<string, Control> Controls { get; set; }
+        string Enabled { get; set; }
+        void Apply(float value);
+        void Draw();
+    }
+}
+
